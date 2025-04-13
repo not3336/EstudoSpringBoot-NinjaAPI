28ddbd6fc830fd6112d1a71c07e5fda4fbfbf680
rafael2044 <rafaelsoaresgomes1999@gmail.com>
2025-04-13 15:15:55 -0300

    first commit

---
+++ src/main/java/com/rafael/NinjaAPI/NinjaApiApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.NinjaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafael/NinjaAPI/NinjaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.NinjaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
