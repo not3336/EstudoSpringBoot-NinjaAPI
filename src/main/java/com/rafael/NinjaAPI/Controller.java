338da371d7fc7105b961c00558ed8d7a6f561d57
rafael2044 <rafaelsoaresgomes1999@gmail.com>
2025-04-13 15:38:27 -0300

    Criado a primeira Controller da aplicação e a rota /boasvindas (apenas para testar se a aplicação está funcionando)

---
+++ src/main/java/com/rafael/NinjaAPI/Controller.java
@@ -0,0 +1,14 @@
+package com.rafael.NinjaAPI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/boasvindas")
+    public String mensagemBoasVindas(){
+        return "Seja bem-vindo a primeira roda da API Ninja";
+    }
+}
