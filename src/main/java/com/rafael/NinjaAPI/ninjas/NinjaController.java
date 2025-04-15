913f8252dd9fb78bdbb0d9a5410276021e19bb11
rafael2044 <rafaelsoaresgomes1999@gmail.com>
2025-04-14 21:22:45 -0300

    feat: adiciona os serviços de resgatar, inserir, atualizar e excluir de ninjas. Os endPoints ninjas foram adicionados com a logica de serviço

--- src/main/java/com/rafael/NinjaAPI/ninjas/NinjaController.java
+++ src/main/java/com/rafael/NinjaAPI/ninjas/NinjaController.java
@@ -1,14 +1,41 @@
 package com.rafael.NinjaAPI.ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/ninjas")
 public class NinjaController {
-    @GetMapping("/boasvindas")
-    public String mensagemBoasVindas(){
-        return "Seja bem-vindo a primeira roda da API Ninja";
+    @Autowired
+    private NinjaService ninjaService;
+
+    @GetMapping
+    public ResponseEntity<List<NinjaModel>> findALl(){
+        return ResponseEntity.ok(ninjaService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<NinjaModel> findById(@PathVariable Long id){
+        return ResponseEntity.ok(ninjaService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<NinjaModel> register(@RequestBody Map<String, String> request){
+        return ResponseEntity.ok(ninjaService.save(request));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<NinjaModel> update(@PathVariable Long id, @RequestBody Map<String, String> request){
+        return ResponseEntity.ok(ninjaService.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        ninjaService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/rafael/NinjaAPI/ninjas/NinjaService.java
+++ src/main/java/com/rafael/NinjaAPI/ninjas/NinjaService.java
@@ -1,7 +1,73 @@
 package com.rafael.NinjaAPI.ninjas;
 
+import com.rafael.NinjaAPI.missions.MissionModel;
+import com.rafael.NinjaAPI.missions.MissionRepository;
+import com.rafael.NinjaAPI.missions.MissionService;
+import com.rafael.NinjaAPI.ninjas.exceptions.EmailAlreadyExistsException;
+import com.rafael.NinjaAPI.ninjas.exceptions.NinjaNotFoundExecption;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class NinjaService {
+
+    private final NinjaRepository ninjaRepository;
+    private final MissionService missionService;
+
+    public NinjaService(NinjaRepository ninjaRepository, MissionService missionService) {
+        this.ninjaRepository = ninjaRepository;
+        this.missionService = missionService;
+    }
+
+    public List<NinjaModel> findAll(){
+        return ninjaRepository.findAll();
+    }
+
+    public NinjaModel findById(Long id) throws NinjaNotFoundExecption{
+        var exists = ninjaRepository.findById(id);
+        if (exists.isEmpty()){
+            throw new NinjaNotFoundExecption("Ninja with ID " + id + " Not Found");
+        }
+        return exists.get();
+    }
+
+    public NinjaModel save(Map<String, String> request){
+        MissionModel mission = missionService.findById(Long.parseLong(request.get("mission_id")));
+        NinjaModel ninja = new NinjaModel(
+                null,
+                request.get("name"),
+                request.get("email"),
+                request.get("img_url"),
+                Integer.parseInt(request.get("age")),
+                request.get("rank"),
+                mission);
+        return ninjaRepository.save(ninja);
+    }
+
+    public NinjaModel update(Long id, Map<String, String> request){
+        var exists = ninjaRepository.findById(id);
+        if (exists.isEmpty()){
+            throw new NinjaNotFoundExecption("Ninja with ID " + id + " Not Found");
+        }
+        if (ninjaRepository.findByEmail(request.get("email")).isPresent()){
+            throw new EmailAlreadyExistsException("there is already a ninja with this email");
+        }
+        NinjaModel ninja = exists.get();
+        ninja.setName(request.get("name"));
+        ninja.setEmail(request.get("email"));
+        ninja.setAge(Integer.parseInt(request.get("age")));
+        ninja.setRank(request.get("rank"));
+        return ninjaRepository.save(ninja);
+    }
+
+    public void delete(Long id){
+        var exists = ninjaRepository.findById(id);
+        if (exists.isEmpty()){
+            throw new NinjaNotFoundExecption("Ninja with ID " + id + " not found");
+        }
+        ninjaRepository.delete(exists.get());
+    }
 }
