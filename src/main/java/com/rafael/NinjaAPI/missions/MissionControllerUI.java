8bab4680127d1ce80a4bf10723995a84cfe8b0f7
rafael2044 <rafaelsoaresgomes1999@gmail.com>
2025-04-20 10:52:56 -0300

    feat: Cria a NinjaControllerUI e MissionControllerUI e configurar o @RequestMapping

---
+++ src/main/java/com/rafael/NinjaAPI/missions/MissionControllerUI.java
@@ -0,0 +1,9 @@
+package com.rafael.NinjaAPI.missions;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/missions/ui")
+public class MissionControllerUI {
+}
