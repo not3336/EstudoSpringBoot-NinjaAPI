e67254be4b8c019449078e8d1271c03d71a2358a
rafael2044 <rafaelsoaresgomes1999@gmail.com>
2025-04-20 15:13:34 -0300

    doc: Adiciona Swagger na aplicação para documentação das controllers Ninja e Mission

--- src/main/java/com/rafael/NinjaAPI/missions/MissionController.java
+++ src/main/java/com/rafael/NinjaAPI/missions/MissionController.java
@@ -1,5 +1,9 @@
 package com.rafael.NinjaAPI.missions;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,26 +20,64 @@ public class MissionController {
     }
 
     @GetMapping
-    public ResponseEntity<List<MissionDto>> findAll(){
+    @Operation(summary = "list all missions", description = "Returns a list of all missions")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Missions successfully found"),
+            @ApiResponse(responseCode = "404", description = "Mission not found")
+    })
+    public ResponseEntity<List<MissionDto>> findAll()
+    {
         return ResponseEntity.ok(missionService.findAll());
     }
+
     @GetMapping("/{id}")
-    public ResponseEntity<MissionDto> findById(@PathVariable Long id){
+    @Operation(summary = "returns a mission by id", description = "Returns the mission with the specified Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Mission successfully found"),
+            @ApiResponse(responseCode = "404", description = "Mission not found")
+    })
+    public ResponseEntity<MissionDto> findById(
+            @Parameter(description = "The user sends the id in the request path")
+            @PathVariable Long id)
+    {
         return ResponseEntity.ok(missionService.findById(id));
     }
 
     @PostMapping
-    public ResponseEntity<MissionDto> register(@RequestBody MissionDto missionDto){
+    @Operation(summary = "Register a mission", description = "Registers a mission in the database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Mission registered successfully"),
+            @ApiResponse(responseCode = "400", description = "Error registered mission")
+    })
+    public ResponseEntity<MissionDto> register(@RequestBody MissionDto missionDto)
+    {
         return ResponseEntity.ok(missionService.save(missionDto));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<MissionDto> update(@PathVariable Long id, @RequestBody MissionDto updateMissionDto){
+    @Operation(summary = "Change a mission", description = "Registers a mission with specified id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Mission changed successfully"),
+            @ApiResponse(responseCode = "400", description = "Error changed mission")
+    })
+    public ResponseEntity<MissionDto> update(
+            @Parameter(description = "The user sends the id in the request path")
+            @PathVariable Long id,
+            @RequestBody MissionDto updateMissionDto)
+    {
         return ResponseEntity.ok(missionService.update(id, updateMissionDto));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
+    @Operation(summary = "Delete a mission", description = "Deleted a mission with specified id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Mission deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Mission not found")
+    })
+    public ResponseEntity<Void> delete(
+            @Parameter(description = "The user sends the id in the request path")
+            @PathVariable Long id)
+    {
         missionService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
--- src/main/java/com/rafael/NinjaAPI/ninjas/NinjaController.java
+++ src/main/java/com/rafael/NinjaAPI/ninjas/NinjaController.java
@@ -1,5 +1,9 @@
 package com.rafael.NinjaAPI.ninjas;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,29 +18,61 @@ public class NinjaController {
     private NinjaService ninjaService;
 
     @GetMapping()
+    @Operation(summary = "List all ninja", description = "Returns a list of all ninjas registered in the database")
     public ResponseEntity<List<NinjaDto>> findALl(){
         return ResponseEntity.ok(ninjaService.findAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<NinjaDto> findById(@PathVariable Long id){
+    @Operation(summary = "Returns a ninja", description = "Returns a ninja with a specific ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ninja successfully found"),
+            @ApiResponse(responseCode = "404", description = "Ninja not found")
+    })
+    public ResponseEntity<NinjaDto> findById(
+            @Parameter(description = "The user sends the id in the request path")
+            @PathVariable Long id)
+    {
         return ResponseEntity.ok(ninjaService.findById(id));
     }
 
     @PostMapping()
-    public ResponseEntity<String> register(@RequestBody NinjaDto ninjaDto){
+    @Operation(summary = "register a new ninja", description = "Receives the data and registers a new ninja in the database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Ninja created successfully"),
+            @ApiResponse(responseCode = "400", description = "Error creating ninja")
+    })
+    public ResponseEntity<String> register(@RequestBody NinjaDto ninjaDto)
+    {
         NinjaDto ninja = ninjaService.save(ninjaDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body("Successfully created ninja: \nName: " + ninja.getName() + "\nID: " + ninja.getId());
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<NinjaDto> update(@PathVariable Long id, @RequestBody NinjaDto updateNinja){
+    @Operation(summary = "Change ninja by id", description = "Change ninja data by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ninja successfully changed"),
+            @ApiResponse(responseCode = "404", description = "Ninja not found")
+    })
+    public ResponseEntity<NinjaDto> update(
+            @Parameter(description = "The user sends the id in the request path")
+            @PathVariable Long id,
+            @RequestBody NinjaDto updateNinja)
+    {
         return ResponseEntity.ok(ninjaService.update(id, updateNinja));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> delete(@PathVariable Long id){
+    @Operation(summary = "Delete a ninja by id", description = "Delete specific ID ninja from database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Ninja successfully deleted"),
+            @ApiResponse(responseCode = "404", description = "Ninja not found")
+    })
+    public ResponseEntity<String> delete(
+            @Parameter(description = "The user sends the id in the request path")
+            @PathVariable Long id)
+    {
         ninjaService.delete(id);
         return ResponseEntity.ok("Ninja with ID " + id + " deleted successfully");
     }
